743bf7e43dbef83eba462fe321623cbf10eb511b
120210727 LEONARDO ANDRADE CAVALCANTI FILHO <leonardo.cavalcanti.filho@lcc3-116.lcc.ufcg.edu.br>
2023-05-10 09:41:15 -0300

    adicao do R06

---
+++ R06-02-Rot-Linked-list-environment/src/main/java/adt/linkedList/SingleLinkedListImpl.java
@@ -0,0 +1,79 @@
+package adt.linkedList;
+
+public class SingleLinkedListImpl<T> implements LinkedList<T> {
+
+	protected SingleLinkedListNode<T> head;
+
+	public SingleLinkedListImpl() {
+		this.head = new SingleLinkedListNode<T>();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return head == null;
+	}
+
+	@Override
+	public int size() {
+		int aux = 0;
+		while(head.isNIL()){
+			aux ++;
+			head.getNext();
+		}
+		return aux;
+	
+	}
+
+	@Override
+	public T search(T element) {
+		SingleLinkedListNode<T> auxHead = this.head;
+		while(auxHead.isNIL() && auxHead.data!=element){
+			auxHead = auxHead.next;
+
+		}
+		return auxHead.getData();
+		
+	}
+
+	@Override
+	public void insert(T element) {
+		SingleLinkedListNode<T> auxHead = this.head;
+		
+		if(head.isNIL()){
+			SingleLinkedListNode<T> newHead = new SingleLinkedListNode<>(element, new SingleLinkedListNode<T>());
+			this.head = newHead;
+		
+		}else{
+			
+			while(!auxHead.isNIL()){
+				auxHead = auxHead.next;
+			}
+			auxHead.data = element;
+			auxHead.next = new SingleLinkedListNode<>(element, head);
+			head = auxHead;
+
+		}
+		
+	}
+
+	@Override
+	public void remove(T element) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented!");
+	}
+
+	@Override
+	public T[] toArray() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented!");
+	}
+
+	public SingleLinkedListNode<T> getHead() {
+		return head;
+	}
+
+	public void setHead(SingleLinkedListNode<T> head) {
+		this.head = head;
+	}
+
+}
